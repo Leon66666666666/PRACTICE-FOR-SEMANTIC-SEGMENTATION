37a1a4ab800921ff6cc019bcd62fd851d09aaa21
Leon <73463335+Leon66666666666@users.noreply.github.com>
2021-05-01 15:32:35 +0100

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time : 2021/1/3
+# @Author : v
+# @File : predict.py
+# @Software: PyCharm
+import mobilenet
+import numpy as np
+import os, copy
+from PIL import Image
+
+import cv2
+
+import random
+import numpy as np
+np.set_printoptions(threshold=np.inf)
+HEIGHT = 416  # 图像的长
+WIDHT = 416  # 图像的宽
+CLASS_NUMBERS = 32  # 分类数
+# class_colors = [[0, 255, 0], [255, 0, 0],[0, 0, 255],[111, 74, 0],[70, 70, 70],[128, 64, 128],[0, 0, 0],[102, 102, 156],[190, 153, 153],[150, 100, 100],[107, 142, 35],
+#                  [152, 251, 152],[70, 130, 180],[220, 220, 0],[119, 11, 32],[215, 166, 66],[66, 88, 99],[154, 25, 244],[10, 155, 83],]
+
+
+#def generateColorGradient(RGB1,RGB2,n):
+    # dRGB=[float(x2-x1)/(n-1) for x1,x2 in zip(RGB1,RGB2)]
+    # gradient=[tuple([int(x+k*dx) for x, dx in zip(RGB1,dRGB)]) for k in range(n)]
+    # return gradient
+#class_colors=generateColorGradient((0,0,0),(250,244,233),19)
+
+A=np.random.randint(0,255,size=[32,1,3])
+B=A.flatten()
+
+# def list_of_groups(init_list,n):
+#     list_of_groups=zip(*(iter(init_list),)*n)
+#     end_list=[list(i) for i in list_of_groups] # i is a tuple
+#     count = len(init_list)%n
+#     end_list.append(init_list[-count:]) if count != 0 else end_list
+#     return end_list
+#
+# class_colors=list_of_groups(B,3)
+# print(class_colors)
+class_colors = [[51, 137, 191], [176, 26, 245], [104, 166, 230], [53, 4, 201], [233, 126, 160], [134, 13, 225],
+[119, 151, 244], [83, 224, 54], [215, 23, 111], [42, 133, 190], [190, 81, 58], [243, 206, 166], [207, 67, 143],
+[180, 184, 183], [110, 204, 51], [207, 109, 48], [237, 168, 61], [200, 56, 32], [47, 53, 230], [96, 83, 218], [73, 44, 123],
+[11, 75, 46], [177, 11, 233], [204, 220, 112], [250, 104, 45], [116, 21, 109], [20, 164, 30], [2, 71, 82], [113, 90, 229], [84, 189, 204],
+[146, 146, 160], [125, 215, 33]]
+
+#print(class_colors)
+
+def get_model():
+    """ 加载模型和参数"""
+    # 获取模型
+    model = mobilenet.main()
+
+    # 加载参数
+    # model.load_weights("callbacks/ep044-loss0.030-val_loss0.028.h5")
+    model.load_weights('callbacks/mobile2021322.h5')
+    return model
+
+
+def precess_img(img):
+    """
+     对图像进行预处理，使其满足输入数据的要求
+     1， 改变大小，2. 数据归一化 3. 改变shape
+    :param img: 要预处理的图像
+    :return: 返回预处理后的数组
+    """
+
+    # 对测试图像进行预处理，以适应模型的输入
+
+    test_img = img.resize((HEIGHT, WIDHT))  # 改变图像大小-> (416,416)
+    test_img_array = np.array(test_img)  # 图像变成数组
+    test_img_array = test_img_array / 255  # 归一化
+    # print(test_img_array.shape)
+    # (416,416,30 -> (1,416,416,3)
+    test_img_array = test_img_array.reshape(-1, HEIGHT, WIDHT, 3)
+    # print(test_img_array.shape)
+
+    return test_img_array
+
+
+def predicting(model):
+    """ 预测"""
+    # 获取测试图片
+    test_data_path = "datasets/dataset/testData/img"  # 测图片的文件夹
+    test_data = os.listdir(test_data_path)  # 读取测试图片
+    print(test_data)
+
+    for test_name in test_data:
+        # 每个测试图片的具体路径
+        test_img_full_path = os.path.join(test_data_path, test_name)
+        # print(test_img_full_path)
+        test_img = Image.open(test_img_full_path)  # 打开图片
+        # test_img.show()
+        old_test_img = copy.deepcopy(test_img)  # 复制图像 ， 备份
+        test_img_array = np.array(test_img)  # 图片转成数组
+        # print(test_img_array.shape)
+        original_height = test_img_array.shape[0]  # 获得图像的长 600
+        original_width = test_img_array.shape[1]  # 获得图像的宽 800
+
+        test_img_array = precess_img(test_img)  # 图片预处理
+        # (1,208*208,2) -> (208*208,2)
+        predict_picture = model.predict(test_img_array)[0]  # 预测
+        # (208 * 208, 2) -> (208,208,2)
+        predict_picture = predict_picture.reshape((int(HEIGHT / 2), int(WIDHT / 2), CLASS_NUMBERS))
+        # (208,208,2) -> (208,208)两个图层对应位置比较，保存最大的索引
+        # 通道数为2，所以里面保存的是 0或 1
+        predict_picture = predict_picture.argmax(axis=-1)  # 可以理解为合并的图层
+        c = Image.fromarray(np.uint8(predict_picture))
+        save_path = 'Datasets/dataset/testData'
+        save_path = os.path.join(save_path, 'predict_mobile')
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
+
+        c.save(save_path + '/' + test_name)
+        # print(predict_picture.shape)
+
+        # 下面要做的是对合并的图层，分层
+        seg_img_array = np.zeros((int(HEIGHT / 2), int(WIDHT / 2), 3))  # (208,208,3) 值全为0
+
+        colors = class_colors
+
+        # 根据一张合并的图层（predict_picture) 将不同的类赋予不同的颜色
+        for cn in range(CLASS_NUMBERS):
+            # seg_img 0通道
+            seg_img_array[:, :, 0] += ((predict_picture[:, :] == cn) * colors[cn][0]).astype('uint8')
+            # seg_img 1 通道
+            seg_img_array[:, :, 1] += ((predict_picture[:, :] == cn) * colors[cn][1]).astype('uint8')
+            # segm_img 2 通道
+            seg_img_array[:, :, 2] + ((predict_picture[:, :] == cn) * colors[cn][2]).astype('uint8')
+
+        # 数组 转换成 图片
+        seg_img = Image.fromarray(np.uint8(seg_img_array))  # (208,208,3)
+        # seg_img.show()
+        # print(seg_img.size)
+        # 恢复图像大小 (208,208,3) ->(600,800) 方便和原图叠加
+        seg_img = seg_img.resize((original_width, original_height))
+
+        # 合并图像
+        print(old_test_img.size)
+        print(seg_img.size)
+        image = Image.blend(old_test_img, seg_img, 3)
+
+        # 保存图片
+        save_path = 'Datasets/dataset/testData'
+        save_path = os.path.join(save_path, 'imgout_mobile')
+        if not os.path.exists(save_path):
+            os.mkdir(save_path)
+
+        image.save(save_path + '/' + test_name)
+        # break
+
+
+def main():
+    """ 模型预测"""
+    model = get_model()
+    predicting(model)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,140 @@
+import numpy as np
+from PIL import Image
+import cv2
+import sys
+import models
+import numpy as np
+np.set_printoptions(threshold=np.inf)
+import csv
+
+CLASS_NUMBERS = 16
+HEIGHT = 416
+WIDHT = 416
+
+#with open(r'data.txt') as f:
+    #lines = f.readlines()
+    #a = ''
+    #for line in lines:
+        #a+=line.strip()
+    #c = a.split()
+    #b = ''.join(c)
+
+# g=[[1,2,3],
+#    [1,7,3]]
+# m=np.sum(g,axis=0)
+# print(m)
+
+img_predict = Image.open('Datasets/dataset/testData/predict_mobileaug/0001TP_007440.png')
+img_predict = img_predict.resize((960, 720))
+img_array_predict = np.array(img_predict)
+#img_predict = img_predict[:,:,0]
+print(img_array_predict.shape)
+C = img_array_predict.flatten()
+print(min(C))
+print(max(C))
+
+img = Image.open('Datasets/dataset/trainData/label/0001TP_007440_P.png')
+#img = img.resize((int(WIDHT / 2), int(HEIGHT / 2)))
+
+img_array = np.array(img)
+
+
+print(img_array.shape)
+
+#img_array= img_array[:,:,0]
+B = img_array.flatten()
+#print(B)
+
+f_score=np.zeros((16,1))
+
+
+class Evaluation(object):
+    def __init__(self, numClass):
+        self.numClass = numClass
+        self.confusionMatrix = np.zeros((self.numClass,) * 2)
+
+    def pixelAccuracy(self):
+        # return all class overall pixel accuracy
+        #  PA = acc = (TP + TN) / (TP + TN + FP + TN)
+        acc = np.diag(self.confusionMatrix).sum() / self.confusionMatrix.sum()
+        return acc
+
+    def precision(self):
+        # return each category pixel accuracy(A more accurate way to call it precision)
+        # acc = (TP) / TP + FP
+        classAcc = np.diag(self.confusionMatrix) / self.confusionMatrix.sum(axis=1)
+        return classAcc  # 返回的是一个列表值，如：[0.90, 0.80, 0.96]，表示类别1 2 3各类别的预测准确率
+
+    def Recall(self):
+        Re=np.diag(self.confusionMatrix)/self.confusionMatrix.sum(axis=0)
+        return Re
+
+    def meanPixelAccuracy(self):
+        classAcc = self.precision()
+        meanAcc = np.nanmean(classAcc)  # np.nanmean 求平均值，nan表示遇到Nan类型，其值取为0
+        return meanAcc  # 返回单个值，如：np.nanmean([0.90, 0.80, 0.96, nan, nan]) = (0.90 + 0.80 + 0.96） / 3 =  0.89
+
+    def meanIntersectionOverUnion(self):
+        # Intersection = TP Union = TP + FP + FN
+        # IoU = TP / (TP + FP + FN)
+        intersection = np.diag(self.confusionMatrix)  # 取对角元素的值，返回列表
+        union = np.sum(self.confusionMatrix, axis=1) + np.sum(self.confusionMatrix, axis=0) - np.diag(
+            self.confusionMatrix)  # axis = 1表示混淆矩阵行的值，返回列表； axis = 0表示取混淆矩阵列的值，返回列表
+        IoU = intersection / union  # 返回列表，其值为各个类别的IoU
+        mIoU = np.nanmean(IoU)  # 求各类别IoU的平均
+        return mIoU
+
+    def genConfusionMatrix(self, imgPredict, imgLabel):  # 同FCN中score.py的fast_hist()函数
+        # remove classes from unlabeled pixels in gt image and predict
+        mask = (imgLabel >= 0) & (imgLabel < self.numClass)
+        label = self.numClass * imgLabel[mask] + imgPredict[mask]
+        count = np.bincount(label, minlength=self.numClass ** 2)
+        confusionMatrix = count.reshape(self.numClass, self.numClass)
+        print(confusionMatrix)
+        datas=confusionMatrix
+        with open("./user_info.csv","w") as f:
+            writer=csv.writer(f)
+            for row in datas:
+                writer.writerow(row)
+        return confusionMatrix
+
+
+
+    def Frequency_Weighted_Intersection_over_Union(self):
+        # FWIOU =     [(TP+FN)/(TP+FP+TN+FN)] *[TP / (TP + FP + FN)]
+        freq = np.sum(self.confusionMatrix, axis=1) / np.sum(self.confusionMatrix)
+        iu = np.diag(self.confusionMatrix) / (
+                np.sum(self.confusionMatrix, axis=1) + np.sum(self.confusionMatrix, axis=0) -
+                np.diag(self.confusionMatrix))
+        FWIoU = (freq[freq > 0] * iu[freq > 0]).sum()
+        return FWIoU
+
+    def addBatch(self, imgPredict, imgLabel):
+        assert imgPredict.shape == imgLabel.shape
+        self.confusionMatrix += self.genConfusionMatrix(imgPredict, imgLabel)
+
+    def reset(self):
+        self.confusionMatrix = np.zeros((self.numClass, self.numClass))
+
+
+if __name__ == '__main__':
+    imgPredict = C  # predict
+    imgLabel = B  # groundTruth
+    metric = Evaluation(16)  # number of classification
+    metric.addBatch(imgPredict, imgLabel)
+    pa = metric.pixelAccuracy()
+    precision = metric.precision()
+    mpa = metric.meanPixelAccuracy()
+    mIoU = metric.meanIntersectionOverUnion()
+    recall = metric.Recall()
+    for i in range(0,16):
+        f_score[i]= (2 * precision[i] * recall[i]) / (precision[i] + recall[i])
+    print(f_score)
+    print(len(f_score))
+    print('pa is : %f' % pa)
+    print('cpa is :')  # 列表
+    print(precision)
+    print('mpa is : %f' % mpa)
+    print('mIoU is : %f' % mIoU)
+    print('recall is: ')
+    print(recall)
